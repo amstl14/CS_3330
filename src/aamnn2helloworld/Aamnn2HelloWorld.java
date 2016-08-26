08df445ef729ad8af68b1989994fbc33721ae399
amstl14 <ADK@Arts-MacBook-Pro.local>
2016-08-26 02:04:12 -0500

    Hello World

---
+++ src/aamnn2helloworld/Aamnn2HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aamnn2helloworld;
+
+/**
+ *
+ * @author ADK
+ */
+public class Aamnn2HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello, World!");
+        
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
